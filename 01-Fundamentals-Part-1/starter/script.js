6933602a9d07f95932575dcb4c5e9963671e4448
Emilina Sanduleac <e.codreanu@students.clark.edu>
2022-08-29 15:44:18 -0400

    initial

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log("23");
+
+let firstName = "Mark";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "james"
