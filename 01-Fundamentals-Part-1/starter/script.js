d09038569e45e40a0e1c2dd60789767a04a7e907
Emilina Sanduleac <e.codreanu@students.clark.edu>
2022-08-31 17:00:51 -0400

    learning

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -13,4 +14,85 @@ console.log(firstName);
 //im here //imhere second
 let jonas_matilda = "JM";
 let $function = 27;
+
 let person = "james"
+let PI = 3.14159;
+
+let myFirstJob = 'Programer';
+let myCurentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+consule.log(myFirstJob);
+*/
+
+// let jasascriptIsFrun = true;
+// console.log(jasascriptIsFrun);
+
+// console.log(typeof true);
+// console.log(typeof jasascriptIsFrun);
+//  console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// jasascriptIsFrun = 'YES!';
+// console.log(typeof jasascriptIsFrun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// // const job;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// lastName = 'Codreanu';
+// console.log(lastName);
+
+// Math Operators
+// const now = 2037
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Emilina';
+// const lastName = 'Codreanu';
+// console.log(firstName + " " + lastName);
+
+// // Assignment Operators
+// let x = 10 + 5; //15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1 = 101
+// x--; // x = x - 1 = 100
+// x--; // x = x - 1 = 99
+// console.log(x);
+
+
+// // Comperison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageJonas < ageSarah);
+// console.log(ageJonas >= ageSarah);
+// console.log(ageJonas <= ageSarah);
+
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018)
+
---
+++ 01-Fundamentals-Part-1/starter/script2.js
@@ -0,0 +1,41 @@
+// // Assignment 1
+
+let country = 'United States';
+let continent = 'North America';
+let population = '329.5';
+console.log(country);
+console.log(continent)
+console.log(population)
+
+// // Assignment 2
+
+// let isIsland = false;
+let language;
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// Assignment 3
+
+language = 'English';
+// const country = 'United States';
+// const continent = 'North America';
+// let isIsland = false;
+// isIsland = true;
+
+// Assignment 4
+
+console.log(population / 2);
+population++;
+console.log(population > 6);
+console.log(population > 33);
+let description =
+    country +
+    ' is in ' +
+    continent +
+    ' and its ' +
+    population +
+    ' million people speak ' +
+    language;
+console.log(description);
