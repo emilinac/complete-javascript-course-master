626382cb6dab3f2e4922a59715160c7c1fcc655f
“Emilina <e.codreanu@students.clark.edu>
2022-09-12 12:53:58 -0400

    commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -62,10 +62,10 @@ consule.log(myFirstJob);
 // console.log(lastName);
 
 // Math Operators
-// const now = 2037
-// const ageJonas = now - 1991;
-// const ageSarah = now - 2018;
-// console.log(ageJonas, ageSarah);
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
 
 // console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
@@ -85,14 +85,31 @@ consule.log(myFirstJob);
 
 
 // // Comperison operators
-// console.log(ageJonas > ageSarah); // >, <, >=, <=
-// console.log(ageJonas < ageSarah);
-// console.log(ageJonas >= ageSarah);
-// console.log(ageJonas <= ageSarah);
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageJonas < ageSarah);
+console.log(ageJonas >= ageSarah);
+console.log(ageJonas <= ageSarah);
 
 // console.log(ageSarah >= 18);
 
 // const isFullAge = ageSarah >= 18;
 
-// console.log(now - 1991 > now - 2018)
+// console.log(now - 1991 > now - 2018);
+
+
+// const now = 2037
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// // console.log(25 - 10 - 5);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2
+// console.log(ageJonas, ageSarah, averageAge);
+
 
--- 01-Fundamentals-Part-1/starter/script2.js
+++ 01-Fundamentals-Part-1/starter/script2.js
@@ -1,16 +1,16 @@
 // // Assignment 1
 
-let country = 'United States';
-let continent = 'North America';
-let population = '329.5';
-console.log(country);
-console.log(continent)
-console.log(population)
+// let country = 'United States';
+// let continent = 'North America';
+// let population = '329.5';
+// console.log(country);
+// console.log(continent)
+// console.log(population)
 
 // // Assignment 2
 
 // let isIsland = false;
-let language;
+// let language;
 // console.log(typeof isIsland);
 // console.log(typeof population);
 // console.log(typeof country);
@@ -18,7 +18,7 @@ let language;
 
 // Assignment 3
 
-language = 'English';
+// language = 'English';
 // const country = 'United States';
 // const continent = 'North America';
 // let isIsland = false;
@@ -26,16 +26,51 @@ language = 'English';
 
 // Assignment 4
 
-console.log(population / 2);
-population++;
-console.log(population > 6);
-console.log(population > 33);
-let description =
-    country +
-    ' is in ' +
-    continent +
-    ' and its ' +
-    population +
-    ' million people speak ' +
-    language;
-console.log(description);
+// console.log(population / 2);
+// population++;
+// console.log(population > 6);
+// console.log(population > 33);
+// let description =
+//     country +
+//     ' is in ' +
+//     continent +
+//     ' and its ' +
+//     population +
+//     ' million people speak ' +
+//     language;
+// console.log(description);
+
+
+
+// Coding Challenge #1
+
+// Data 1
+
+let markMass1 = 79;
+let markHeight1 = 1.69;
+let johnMass1 = 92;
+let johnHeight1 = 1.95;
+
+let markBMI = markMass1 / markHeight1 ** 2;
+let johnBMI = johnMass1 / johnHeight1 ** 2;
+console.log(markBMI);
+console.log(johnBMI);
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+
+// Data 2 
+
+let markMass2 = 95;
+let markHeight2 = 1.88;
+let johnMass2 = 85;
+let johnHeight2 = 1.76;
+
+let markBMI2 = markMass2 / markHeight2 ** 2;
+let johnBMI2 = johnMass2 / johnHeight2 ** 2;
+console.log(markBMI2);
+console.log(johnBMI2);
+
+let johnHigherBMI = markBMI2 < johnBMI2;
+console.log(johnHigherBMI);
